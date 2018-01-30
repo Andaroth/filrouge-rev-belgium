ad09cc3eb938de41bfedd93ef90fad5bc3457004
Unknown <thomaspoint@gmail.com>
2018-01-30 14:32:18 +0100

    Acceuil

--- entdevoix/src/MainComponents/About/About.js
+++ entdevoix/src/MainComponents/About/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class About extends React.Component {
     render() {
         return (
-            <h1>A propos</h1>
+            <div className="Container">
+                <h1>A propos</h1>
+            </div>
         )
     }
 }
--- entdevoix/src/MainComponents/Contact/ContactForm.js
+++ entdevoix/src/MainComponents/Contact/ContactForm.js
@@ -9,7 +9,7 @@ export default class ContactForm extends React.Component {
                 <Row className="contactForm">
                     <Input s={6} label="Name" validate><Icon>account_circle</Icon></Input>
                     <Input s={12} label="Email" validate type='email'><Icon>mail</Icon></Input>
-                    <Input s={12} label="Votre Message" validate type='email'><Icon>message</Icon></Input>
+                    <Input s={12} label="Votre Message"><Icon>message</Icon></Input>
                 </Row>
                 <ButtonForm/>
             </div>
