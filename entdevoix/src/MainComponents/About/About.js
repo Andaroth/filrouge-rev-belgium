5798d3a8f724d718a8842e8c1582d6d043ee698f
Harimaron <gaetan.ribonnet@outlook.com>
2018-01-31 15:35:51 +0100

    Commit

--- entdevoix/src/MainComponents/About/About.js
+++ entdevoix/src/MainComponents/About/About.js
@@ -3,9 +3,25 @@ import React from 'react';
 export default class About extends React.Component {
     render() {
         return (
-            <div className="Container">
-                <h1>A propos</h1>
-            </div>
+<div className='backgroundAbout'>
+<div className="Container Form containerAcceuil Entendeurs">
+    <h4>Qui sommes-nous ?</h4>
+    <p>Le REV-Belgium, Réseau des Entendeurs de Voix de Belgique, s’inscrit dans un mouvement associatif qui se développe à travers le monde depuis près de 20 ans. Il a été créé à l’image du réseau français sur l’entente de voix revfrance et des autres
+        réseaux francophones (suisse et québécois) ou anglophones hearing-voices ou intervoice, suite aux résultats des recherches et études de pionniers Marius Romme et Sandra Escher. Au sein du réseau qui réunit des personnes qui entendent des voix,
+        des sympathisants mais aussi des professionnels de la santé, les voix ne sont pas considérées comme le symptôme d’une maladie mentale qu’il faudrait supprimer, mais comme une expérience humaine qui n’est pas en soi un problème. En effet, de
+        nombreuses personnes entendent des voix sans pour autant passer par la psychiatrie. Par contre, cette expérience peut être très éprouvante, engendrant angoisse et solitude. C’est pourquoi le REV a développé des groupes d’entraide dans un climat
+        de confiance et de respect des croyances de chacun. Il y est possible d’apprendre à mieux vivre avec ses voix et faire face aux difficultés quotidiennes. Plusieurs groupes se réunissent tous les quinze jours dans différentes villes (Bruxelles,
+        Liège, Namur, Mons). Si vous entendez des voix, ou si vous connaissez des personnes qui entendent des voix et en sont troublées, ce réseau peut vous aider.</p>
+    <h4>Nos objectifs</h4>
+    <p>Nous vous offrons de l’information, notre soutien et compréhension aux personnes qui entendent des voix. Nous organisons des groupes d’entendeurs de voix, qui ont montré leur efficacité à aider l’individu à retrouver son identité et reprendre
+        sa vie en mains. Même parfois après des années de psychiatrie. </p>
+    <h4>Charte d'éthique</h4>
+    <p>Le REV-Belgium a adopté ​une charte d'éthique à la quelle les organisateurs des groupes de parole adhèrent, cette charte est aussi distribuée aux entendeurs de voix vous pouvez consulter <a href="http://www.rev-belgium.org/downloads/charte.pdf">la charte</a>.</p>
+    <h4>Financement</h4>
+    <p>L'organisation d'événements, de colloques, de conférences et de réunions de groupes engendre des frais. Une ASBL a été créée en février 2014, dont voici les <a href="http://www.rev-belgium.org/downloads/charte.pdf">statuts</a>. Votre soutien et vos dons seront toujours les bienvenus sur notre compte : <b>BE89
+        0017 3122 5785.</b></p>
+</div>
+</div>
         )
     }
 }
