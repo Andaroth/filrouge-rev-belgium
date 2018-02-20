f53dfc9e792ae0a17de43788df1b58366689da03
Anda <ax.fiolle@gmail.com>
2018-02-20 16:39:09 +0100

    wip admin materialize forms

--- entdevoix/src/MainComponents/Admin/Admin.js
+++ entdevoix/src/MainComponents/Admin/Admin.js
@@ -21,8 +21,10 @@ export default class Liens extends React.Component {
                             <Row node="article" className="white">
                                 <form action="#">
                                     <h6>Accueil</h6>
-                                    <textarea className="maxw" name="content" id="" cols="30" rows="10">
-                                    </textarea>
+                                    <div className="input-field">
+                                        <textarea className="materialize-textarea maxw" name="content" id="" cols="30" rows="10">
+                                        </textarea>
+                                    </div>
                                     <Row className="center-align">
                                         <Col s={6}>
                                             <button><i className="material-icons">cancel</i></button>
@@ -36,7 +38,7 @@ export default class Liens extends React.Component {
                             <Row node="article" className="white">
                                 <form action="#">
                                     <h6>À propos</h6>
-                                    <textarea className="maxw" name="content" cols="30" rows="30">
+                                    <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                     </textarea>
                                     <Row className="center-align">
                                         <Col s={6}>
@@ -51,7 +53,7 @@ export default class Liens extends React.Component {
                             <Row node="article" className="white">
                                 <form action="#">
                                     <h6>Entendeurs</h6>
-                                    <textarea className="maxw" name="content" cols="30" rows="30">
+                                    <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                     </textarea>
                                     <Row className="center-align">
                                         <Col s={6}>
@@ -66,7 +68,7 @@ export default class Liens extends React.Component {
                             <Row node="article" className="white">
                                 <form action="#">
                                     <h6>Histoire</h6>
-                                    <textarea className="maxw" name="content" cols="30" rows="30">
+                                    <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                     </textarea>
                                     <Row className="center-align">
                                         <Col s={6}>
@@ -87,7 +89,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">Le 8ème congrès international des entendeurs de voix</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Le Congrès mondial sur l’entente de voix est organisé chaque année depuis 2009 conjointement par Intervoice et un Réseau national sur l’entente de voix. Les précédents pays ayant accueilli ce congrès sont les Pays-Bas, le Royaume Uni, l’Italie, l’Australie, la Grèce et l’Espagne. En 2017, il se tiendra aux Etats-Unis.
                                             &#60;&#98;&#114;&#47;&#62;Du 19 au 22 octobre 2016, le REV France a accueilli ce Congrès qui a rassemblé plus de 300 personnes de tous horizons et convictions autour d’une perspective commune : Changer l’Histoire en retissant nos histoires !
                                             </textarea>
@@ -109,7 +111,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">Le 9ème congrès international des entendeurs de voix</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Entendre des voix Etats-Unis est fier de présenter le 9e Congrès mondial Hearing Voices : Une révolution de Unseen voix. Le Congrès aura lieu du 16 au 18 Août 2017 à l' Université de Boston à Boston, Massachusetts, États-Unis.
                                             &#60;&#98;&#114;&#47;&#62;Plus d'infos sur leur site
                                             </textarea>
@@ -132,7 +134,7 @@ export default class Liens extends React.Component {
                             <h5>Modifier la page</h5>
                             <div className="container">
                                 <form action="#">
-                                    <textarea className="maxw" name="content" cols="30" rows="30">
+                                    <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                     </textarea>
                                     <Row className="center-align">
                                         <Col s={6}>
@@ -149,7 +151,7 @@ export default class Liens extends React.Component {
                             <h5>Modifier l'entête</h5>
                             <div className="container">
                                 <form action="#">
-                                    <textarea className="maxw" name="" id="" cols="30" rows="10">
+                                    <textarea className="materialize-textarea maxw" name="" id="" cols="30" rows="10">
                                     Lundi 16 Février 2015 à 17h sur Radio Panik (105.4 FM), l'émission Psylence Radio accueillait le REV-Belgium. Aujourd’hui, l’équipe de PsylenceRadio se transforme en vendeurs de REV. Une petite voix me dit que vous pensez que l’orthographe se perd. Détrompez-vous ! Le REV, ce sont les initiales du Réseau des Entendeurs de Voix. Groupe d’entraide mais pas que, c’est aussi un réseau porteur de voix (tiens ! Encore elles ?) afin de faire évoluer les regards sur ces phénomènes souvent stigmatisés ou facteurs d’exclusion. "Quelle voix de rev" (Radio Panik ~ Psylence 2015)
                                     </textarea>
                                     <Row className="center-align">
@@ -169,7 +171,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">"Prêter l'oreille à ses voix" par Matthieu Cornélis, En Marche, 20 Octobre 2016</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Entendre des voix est traditionnellement présenté comme le symptôme d'une maladie mentale - singulièrement la schizophrénie - qu'il faut à tout prix supprimer. Selon certains thérapeutes, cette action radicale est rarement efficace car elle enferme la personne dans une situation sans issue et l'isole dans sa souffrance. Pour lire l'article cliquer ici
                                             </textarea>
                                             <Row className="center-align">
@@ -190,7 +192,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">World Hearing Voices Congress, Madrid, 2015</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Un de nos membres, Jean-Marc Priels, a participé au 7ème congrès mondial des réseaux d'entendeurs de voix au World Hearing Voices Congress qui s'est tenu à Madrid les 6-7 novembre 2015. Il nous propose ici son compte-rendu; nous l'en remercions.
                                             Pour lire l'article cliquer ici
                                             </textarea>
@@ -212,7 +214,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">Un nouvel article de Philippe LAMBERT, publié dans la revue Moustique (21 octobre 2015)</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Entendre des voix est traditionnellement présenté comme le symptôme d'une maladie mentale - singulièrement la schizophrénie - qu'il faut à tout prix supprimer. Selon certains thérapeutes, cette action radicale est rarement efficace car elle enferme la personne dans une situation sans issue et l'isole dans sa souffrance.
                                             Pour lire l'article cliquer ici
                                             </textarea>
@@ -234,7 +236,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">Un nouvel article de Philippe LAMBERT, publié dans la revue Moustique (21 octobre 2015)</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Entendre des voix est traditionnellement présenté comme le symptôme d'une maladie mentale - singulièrement la schizophrénie - qu'il faut à tout prix supprimer. Selon certains thérapeutes, cette action radicale est rarement efficace car elle enferme la personne dans une situation sans issue et l'isole dans sa souffrance.
                                             Pour lire l'article cliquer ici
                                             </textarea>
@@ -256,7 +258,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">Un nouvel article de Philippe LAMBERT, publié dans la revue Moustique (21 octobre 2015)</div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             Entendre des voix est traditionnellement présenté comme le symptôme d'une maladie mentale - singulièrement la schizophrénie - qu'il faut à tout prix supprimer. Selon certains thérapeutes, cette action radicale est rarement efficace car elle enferme la personne dans une situation sans issue et l'isole dans sa souffrance.
                                             Pour lire l'article cliquer ici
                                             </textarea>
@@ -285,7 +287,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">First </div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             </textarea>
                                             <Row className="center-align">
                                                 <Col s={4}>
@@ -305,7 +307,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">First </div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             </textarea>
                                             <Row className="center-align">
                                                 <Col s={4}>
@@ -325,7 +327,7 @@ export default class Liens extends React.Component {
                                         <div className="collapsible-header">First </div>
                                         <div className="collapsible-body">
                                         <form action="#">
-                                            <textarea className="maxw" name="content" cols="30" rows="30">
+                                            <textarea className="materialize-textarea maxw" name="content" cols="30" rows="30">
                                             </textarea>
                                             <Row className="center-align">
                                                 <Col s={4}>
